655480fd9e02fd7ca43e4ddafb56204c58302343
Fecsx <fecsoandrasbalazs@gmail.com>
2025-09-08 16:58:55 +0200

    aeysrcgbhnml,é

---
+++ EVA/1st_week/DocumentStatistics/DocumentStatistics/DocumentStatistics.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvaWeek1
+{
+    internal class DocumentStatistics
+    {
+        private string _filePath;
+
+        public string fileContent {get; private set;}
+
+        public DocumentStatistics(string filePath)
+        {
+            _filePath = filePath;
+        }
+
+        public void Load()
+        {
+            fileContent = File.ReadAllText(_filePath);
+        }
+        
+        
+    }
+}
---
+++ EVA/1st_week/DocumentStatistics/DocumentStatistics/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace EvaWeek1
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            string filePath;
+            do
+            {
+                filePath = Console.ReadLine() ?? "";
+
+            } while (Path.GetExtension(filePath) != ".txt" || !Path.Exists(filePath));
+
+            DocumentStatistics stat = new DocumentStatistics(filePath);
+
+            try
+            {
+                stat.Load();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"Failed to load file. Message: {ex.Message}");
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
